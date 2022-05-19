8f35fa494867ccacd5494a3f02b67d2032240c21
Yonas Desta <yonasdesta2012@gmail.com>
2022-05-19 14:32:36 +0300

    fixed again

--- main.c
+++ main.c
@@ -1,105 +1,28 @@
-#include "shell.h"
+#include "minishell.h"
 
-static char *FIRST_ARG;
-
-int handle_arguments(int ac, char **av, int *exec_file);
-void sigintHandler(int sig_num);
-char *get_first_av();
 
 /**
- * main - Entry point
- * @ac: number of arguments
- * @av: Array of arguments
+ * main - entry point
  *
- * Return: 0 on success
-*/
-int main(int ac, char **av)
-{
-	int read, exec_file = 0;
-	char *buff = NULL;
-	size_t buff_len = 0;
-	int fd;
-
-	FIRST_ARG = av[0];
-
-	signal(SIGINT, sigintHandler);
-	fd = handle_arguments(ac, av, &exec_file);
-	/*update_count_lines();*/
-
-	while (1)
-	{
-		/* Print console symbol only if it is interactive*/
-		if (isatty(STDIN_FILENO) == 1 && exec_file == 0)
-			write(STDOUT_FILENO, "$ ", 2);
-		/* Read commands from console */
-		/*read = read_line(fd, &buff);*/
-		read = getline(&buff, &buff_len, stdin);
-		if (read == EOF)
-		{
-			free(buff);
-			exit(*process_exit_code());
-		}
-		/*handle_history(buff);*/
-		/* Remove comments & '\n' char from buffer */
-		buff = handle_comment(buff);
-		_strtok(buff, "\n");
-		/* Handling_semicolon, ||, && and executes inside of the function */
-		handling_semicolon_and_operators(buff, read, av[0]);
-	}
-	/* Free buffer memory */
-	free(buff);
-	if (exec_file)
-		close(fd);
-	return (*process_exit_code());
-}
-
-/**
- * handle_arguments - Check the number of arguments passed to main
- * @ac: Number of arguments
- * @av: Array of arguments as strings
- * @exec_file: Integer used to check if user wants to exec commands from file
+ * @argc: number of arguments given to the program
+ * @argv: arguments list
  *
- * Return: File descriptor to file
-*/
-int handle_arguments(int ac, char **av, int *exec_file)
+ * Return: returns the value of the last executed command
+ */
+int main(int argc, char **argv)
 {
-	int fd = STDIN_FILENO;
-	char *err_msg = "Error: more than one argument\n";
+	char *cmdline = NULL;
+	char **cmdargs;
+	char prompt[] = "(hsh) ";
+	Bool status = true;
 
-	if (ac > 2)
+	name = (argv[0] != NULL) ? argv[0] : NULL;
+
+	while (status)
 	{
-		write(STDERR_FILENO, err_msg, _strlen(err_msg));
-		exit(1);
-	}
-	if (ac == 2)
-	{
-		fd = open(av[1], O_RDONLY);
-		*exec_file = 1;
-	}
-	if (fd == -1)
-	{
-		perror(av[0]);
-		exit(1);
+		write(1, prompt, strlen(prompt));
+		cmdline = get_user_input();
 	}
 
-	return (fd);
-}
-
-/**
- * sigintHandler - Avoids current process to finish
- * @sig_num: Signal number
-*/
-void sigintHandler(int __attribute__((unused))sig_num)
-{
-	write(STDIN_FILENO, "\n$ ", 3);
-}
-
-/**
- * get_first_av - Returns the first argument passed to main
- *
- * Return: Pointer to first arg passed to main
-*/
-char *get_first_av(void)
-{
-	return (FIRST_ARG);
+	return (0);
 }
